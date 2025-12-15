c4698d753b25b05ca418dcf85588fd47bd1d0a49
vrawat <rawat011@gmail.com>
2025-12-15 10:42:06 +0530

    use current date instead of hardcoded value. If POTD is not a picture then display yesterday's pic

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
-date = "2025-11-23"
-potd_url = f"https://api.nasa.gov/planetary/apod?api_key=RNSXJd5RsHie92R4SaA9j7h3IChATW9AhoH7RcLA&date={date}"
+import datetime
+today_date = datetime.date.today()
+potd_url = f"https://api.nasa.gov/planetary/apod?api_key=RNSXJd5RsHie92R4SaA9j7h3IChATW9AhoH7RcLA&date={today_date}"
 
 # get potd content
 response = requests.get(potd_url)
@@ -9,12 +10,15 @@ content = response.json()
 
 # download the image
 img_video_url = content["url"]
-response = requests.get(img_video_url)
 
-# save the imgae
-potd = "potd.jpg"
-with open(potd, "wb") as file:
-    file.write(response.content)
+# If not an image, display previous day image
+if img_video_url.endswith(".jpg"):
+    response = requests.get(img_video_url)
+
+    # save the imgae
+    potd = "potd.jpg"
+    with open(potd, "wb") as file:
+        file.write(response.content)
 
 # crate web-page with all the info
 title = content["title"]
